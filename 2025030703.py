1c4b72e2c05b35a6adc8f4a383269b58e7bd6bb2
LKH0613 <lkh120613@gmail.com>
2025-03-14 19:13:48 +0900

    Add files via upload

---
+++ 2025030703.py
@@ -0,0 +1,28 @@
+'''
+input("학교 :")
+input("학년 :")
+input("  반 :")
+print("나는 " + input("학교 :") + "중학교" + input("학년 :") + "학년 " + input("  반 :") + "반"+ "입니다.")
+
+print("\"hello\"")
+print("\n*\n**\n***\n****\n***\n**\n*")
+
+print("\"습관\'보다")
+print("\t강한 것은 없다")
+
+print("\"습관\'보다\n\t강한 것은 없다")
+
+import turtle
+turtle.shape("turtle")
+turtle.circle(200)
+turtle.done
+'''
+import turtle
+#turtle.shape("triangle")
+#turtle.write("Welcome to Python world !" , True , font = ("Arial", 18, "bold"))
+#turtle.circle(18, 210)
+#turtle.done()
+
+turtle.circle(50)
+turtle.circle(100)
+turtle.circle(150)
