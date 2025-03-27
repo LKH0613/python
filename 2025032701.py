f4c7b0e8352cd3de916e68ac0b1fb97fecc9a5ca
LKH0613 <lkh120613@gmail.com>
2025-03-27 21:49:42 +0900

    Add files via upload

---
+++ 2025032701.py
@@ -0,0 +1,20 @@
+import turtle
+t = turtle.Turtle()
+
+t.shape("turtle")
+s = int(input("집의 크기를 입력하세요 : "))
+t.forward(s)        #정사각형
+t.right(90)
+t.forward(s)
+t.right(90)
+t.forward(s)
+t.right(90)
+
+t.forward(s)        #정삼각형
+t.right(30)
+t.forward(s)
+t.right(120)
+t.forward(s)
+t.right(120)
+t.forward(s)
+t.right(180)
